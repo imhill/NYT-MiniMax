adac66fd99b165b26d442d16be706b38ddb022bf
Ian <ianmhill4@gmail.com>
2026-07-02 22:13:11 -0400

    Updated to restore functionality

--- src/scripts/old_content.js
+++ src/scripts/old_content.js
@@ -1,25 +1,14 @@
-// initial hint font size
-const initialFontSize = 20;
-
 //number of miliseconds in a day
 const dayMS = 86400000;
 
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-async function insertTextSizeControls(){
-    await delay(350);
-
-    // add font size control buttons to the control bar
-    // control bar classes: 
-    // xwd__toolbar--expandedMenu
-
+//function to check for the toolbar to be loaded
+function checkForToolbar(){
+    //try to select the toolbar
     const toolbar = document.querySelector("div.xwd__toolbar--expandedMenu");
 
     //if it exists, build the additional features
     if (toolbar) {
-        console.log("found toolbar");
         //create a new list element
-        
         const newToolbarElement = document.createElement("li");
         newToolbarElement.classList.add(...["xwd__tool--button", "xwd__tool--texty"]);
         newToolbarElement.id = "changeSizeTab";
@@ -34,8 +23,7 @@ async function insertTextSizeControls(){
         //create the list for the objects in the tab
         const optionList = document.createElement("ul");
         optionList.className = "xwd__menu--container";
-        optionList.style.width = "120px";
-        optionList.style.fontSize = "20px";
+        optionList.style.width = "100px";
 
         const titleListElement = document.createElement("li");
         titleListElement.classList.add(...["xwd__menu--item", "xwd__menu--item-display"]);
@@ -51,7 +39,7 @@ async function insertTextSizeControls(){
         inputListInput.step = "2";
         inputListInput.addEventListener("change",updateFontSize);
         inputListInput.id = "sizeInput";
-        inputListInput.value = `${initialFontSize}`;
+        inputListInput.value = "14";
         inputListInput.className = "xwd__menu--btnlink";
         inputListInput.style.width = "4em";
 
@@ -65,62 +53,41 @@ async function insertTextSizeControls(){
         newToolbarElement.appendChild(optionList);
 
         toolbar.appendChild(newToolbarElement);
-        
-        updateFontToSize();
-    } else {
-        console.log("failed to add size button to toolbar");
-    }
-}
 
-//function to check for the toolbar to be loaded
-function addToolbar(){
-    // add previous and next buttons to the title and date bar
-    // Title header classes:
-    // xwd__header--row xwd__header--fullwidth
-    const headerContainer = document.querySelector("div.xwd__header--row.xwd__header--fullwidth");
+        //********* 
+        //now add the previous and next puzzle buttons
 
-    // add event to "play" button that adds font size controls
-    // play button classes:
-    //_momentButton_e4jbe_2
-    const playButton = document.querySelector("button._momentButton_e4jbe_2");
-
-    // prev and next buttons
-    if(headerContainer && playButton){        
         currentGameDate = new Date(Date.parse(document.querySelector("div.xwd__details--date").textContent));
         tomorrowDate = new Date(currentGameDate.getTime()+dayMS);
         yesterdayDate = new Date(currentGameDate.getTime()-dayMS);
-
-        const buttonContainer = document.createElement("div");
-        buttonContainer.style.width = "600px";
 
         const tomorrowGameButtonLink = document.createElement("a");
         tomorrowGameButtonLink.href = `https://www.nytimes.com/crosswords/game/mini/${tomorrowDate.toISOString().split("T")[0].replaceAll("-","/")}`;
         const tomorrowGameButton = document.createElement("button");
         tomorrowGameButton.textContent = "Tomorrow's Puzzle";
-        tomorrowGameButton.style.padding = "4px";
-        tomorrowGameButton.style.margin = "10px";
         tomorrowGameButtonLink.appendChild(tomorrowGameButton);
 
         const yesterdayGameButtonLink = document.createElement("a");
         yesterdayGameButtonLink.href = `https://www.nytimes.com/crosswords/game/mini/${yesterdayDate.toISOString().split("T")[0].replaceAll("-","/")}`;
         const yesterdayGameButton = document.createElement("button");
         yesterdayGameButton.textContent = "Yesterday's Puzzle";
-        yesterdayGameButton.style.padding = "4px";
-        yesterdayGameButton.style.margin = "10px";
         yesterdayGameButtonLink.appendChild(yesterdayGameButton);
 
-        buttonContainer.appendChild(yesterdayGameButtonLink);
-        buttonContainer.appendChild(tomorrowGameButtonLink);
+        const puzzleTitle = document.querySelector("div.xwd__details--container");
 
-        headerContainer.appendChild(buttonContainer);
+        const otherGameSelectorDiv = document.createElement("div");
+        otherGameSelectorDiv.appendChild(yesterdayGameButtonLink);
+        otherGameSelectorDiv.appendChild(tomorrowGameButtonLink);
+        otherGameSelectorDiv.style.marginTop = "-20px";
+        otherGameSelectorDiv.style.marginLeft = "345px";
 
-        playButton.addEventListener("click", insertTextSizeControls);
+        puzzleTitle.appendChild(otherGameSelectorDiv);
 
-        console.log("Added all features!");
-
+        //stop searching now that we are done
         stopInterval();
+
     } else {
-        console.log("trying to add features again...");
+        //console.log("OH No");
     }
 }
 
@@ -134,14 +101,6 @@ function updateFontSize(){
     hintList.style.fontSize = `${sizeInput.value}px`;
 }
 
-function updateFontToSize(){
-    //select the hint list element and the input for the font size
-    const hintList = document.querySelector("section.xwd__layout--cluelists");
-
-    //update the font size
-    hintList.style.fontSize = `${initialFontSize}px`;
-}
-
 //function to display the tab when the button is clicked on
 function displaySizeTab(){
     const tab = document.getElementById("changeSizeTab");
@@ -153,13 +112,13 @@ function displaySizeTab(){
     }   
 }
 
-//look for the toolbar every 100 miliseconds
-const searchInterval = setInterval(addToolbar, 100);
+//look for the toolbar every 25 miliseconds
+const searchInterval = setInterval(checkForToolbar, 25);
 
 //function to stop the interval function
 function stopInterval(){
     clearInterval(searchInterval);
 }
 
-//stop searching after a few seconds
-setTimeout(stopInterval, 3000);
+//stop searching after a second
+setTimeout(stopInterval, 1000);
